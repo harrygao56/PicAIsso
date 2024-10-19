5f72bb60d1c308bee3b480436727871a0de950fa
elysiaq <97921607+elysiaq@users.noreply.github.com>
2024-10-19 15:33:53 -0500

    updated cloud storage methods

--- server/app/cloud_storage.py
+++ server/app/cloud_storage.py
@@ -2,10 +2,16 @@ from google.cloud import storage
 import os
 
 PROJECT_ID = 'poetic-bongo-439118-t6'
+storage_client = storage.Client(project=PROJECT_ID)
+bucket_name = "picaissoimages"
+bucket = storage_client.bucket(bucket_name)
 
-def upload_image_to_gcs(image_data, filename, bucket_name):
-    client = storage.Client(project=PROJECT_ID)
-    bucket = client.bucket(bucket_name)
+def upload_image_data_to_gcs(image_data, filename):
     blob = bucket.blob(filename)
     blob.upload_from_string(image_data, content_type='image/jpeg')
+    return blob.public_url
+
+def upload_image_file_to_gcs(image_file, filename):
+    blob = bucket.blob(filename)
+    blob.upload_from_file(image_file)
     return blob.public_url
